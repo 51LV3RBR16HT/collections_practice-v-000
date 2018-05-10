f73a5dd23e26569fedf72c8c01275d91394a1c79
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-10 19:40:14 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -7,7 +7,6 @@ def sort_array_desc(array)
 end
 
 def sort_array_char_count(array)
-  result = array.sort {|left, right|
-  left.length <=> right.length}
+  result = array.sort {|left, right| left.length <=> right.length}
   puts result
 end
