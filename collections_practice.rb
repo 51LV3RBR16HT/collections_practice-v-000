094b9dfabc768c6a8daad4503a067c373cdedce1
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-10 19:47:06 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -8,10 +8,9 @@ end
 
 def sort_array_char_count(array)
   result = array.sort {|left, right| left.length <=> right.length}
+  # here we compare the string lengths, not the strings themselves
   # create a variable 'result' to sort through the array length
   # use the curly brackets to specify the block of code
   # first specify, on the left between vertical bars, the arguments
-  # throw it in two local variables (two is needed for the sort method to work)
   # use the <=> operator to compare the two arguments in the block...
-  # here we compare the string lengths, not the strings themselves
 end
